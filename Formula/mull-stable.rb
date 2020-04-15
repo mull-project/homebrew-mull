927f4400b364effeae05603dbfc611d9bfb8e336
Alex Denisov <alex@lowlevelbits.org>
2020-04-15 22:05:24 +0200

    Release 0.7.0

--- Formula/mull-stable.rb
+++ Formula/mull-stable.rb
@@ -1,15 +1,16 @@
 class MullStable < Formula
-  desc ""
-  homepage ""
+  version "0.7.0"
+  desc "LLVM-based mutation testing for C and C++"
+  homepage "https://github.com/mull-project/mull"
   url "https://dl.bintray.com/mull-project/macos/pool/main/m/Mull-0.7.0-LLVM-9.0-macOS-10.15.3.zip"
   sha256 "ead397a4c57f60337a2cf02394b65e500a60691892ecadd2ae36cba8317b10c6"
 
   def install
-    %[ pwd ]
+    bin.install "bin/mull-cxx"
   end
 
   test do
-    system "false"
+    system bin/"mull-cxx", "--version"
   end
 end
 
